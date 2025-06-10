c1ea36300f787ea619836e549096a6e3609bc016
Praveen Kumar <praveen@example.com>
2025-06-10 16:46:18 +0530

    LAtest

--- src/app/Components/pages.services/business/business.ts
+++ src/app/Components/pages.services/business/business.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from "../../footer/footer";
+import { Header } from "../../header/header";
 
 @Component({
   selector: 'app-business',
-  imports: [],
+  imports: [Footer, Header],
   templateUrl: './business.html',
   styleUrl: './business.css'
 })
--- src/app/Components/pages.services/cloud/cloud.ts
+++ src/app/Components/pages.services/cloud/cloud.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../../header/header";
+import { Footer } from "../../footer/footer";
 
 @Component({
   selector: 'app-cloud',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './cloud.html',
   styleUrl: './cloud.css'
 })
